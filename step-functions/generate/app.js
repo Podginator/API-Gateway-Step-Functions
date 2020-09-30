ae4340f470c6321a70f77540a4db95b6b6436e3b
Thomas Rogers <podginator@gmail.com>
2020-09-30 09:21:04 +0200

    Add more things

---
+++ step-functions/generate/app.js
@@ -0,0 +1,36 @@
+const AWS = require('aws-sdk');
+const bwipjs = require('bwip-js');
+
+const s3 = new AWS.S3();
+
+const getBarcodeImage = (text) => { 
+    return new Promise((resolve, reject) => { 
+        bwipjs.toBuffer({
+            bcid:        'code128',       
+            text,       
+            scale:       3,               
+            height:      10,              
+            includetext: true,            
+            textxalign:  'center',        
+        }, (err, png) => { 
+            if (err) { 
+                return reject(err)
+            }
+            resolve(png)
+        })
+    });
+};
+
+exports.handler = async (event, context, callback) => {
+    const { text, clientId } = event
+
+    const barcodeImage = await getBarcodeImage(text);
+    await s3.putObject({ 
+        Bucket: process.env.S3_BUCKET,
+        Key: `${clientId}_pdf.png`,
+        ACL: 'public-read',
+        Body: barcodeImage
+    }).promise() 
+
+    callback(null, { clientId, url: `https://${process.env.S3_BUCKET}.s3.amazonaws.com/${clientId}_pdf.pdf` });
+};
